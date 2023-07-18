06fef6137442b80c5db62947b8c5e957a6d1773a
yhuzued <yusufmait@gmail.com>
2023-07-18 09:24:27 +0700

    Improve styling

--- src/components/header/Description.jsx
+++ src/components/header/Description.jsx
@@ -10,7 +10,7 @@ function Description({ desc, setDescEdited }) {
     <div className="mt-4">
       <h2 className="text-xl mb-2">Professional Summary</h2>
       <button
-        className="float float-right px-2 py-1 rounded-sm drop-shadow-sm bg-blue-900 text-slate-50"
+        className="float float-right px-2 py-1 rounded-sm drop-shadow-sm bg-blue-900 text-slate-50 hover:bg-blue-700"
         onClick={() => setDescEdited(true)}
       >
         Edit
