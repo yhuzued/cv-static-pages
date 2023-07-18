e5870d3ab0ceea6f9621d7040c626bf0eadb889d
yhuzued <yusufmait@gmail.com>
2023-07-18 13:55:34 +0700

    Styling form experience

--- src/components/experience/FormExperience.jsx
+++ src/components/experience/FormExperience.jsx
@@ -1,19 +1,61 @@
 export default function FormExperience() {
   return (
     <form onSubmit={console.log("submit")} className="my-5">
-      <div>
-        <label htmlFor="job">Job Title</label>
-        <input type="text" name="job" id="job" />
-        <label htmlFor="employer">Employer</label>
-        <input type="text" name="employer" id="employer" />
-        <label htmlFor="cityProvince">City Province</label>
-        <input type="text" name="cityProvince" id="cityProvince" />
-        <label htmlFor="country">Country</label>
-        <input type="text" name="country" id="country" />
-        <label htmlFor="startDate">Start Date</label>
-        <input type="month" name="startDate" id="startDate" />
-        <label htmlFor="endDate">End Date</label>
-        <input type="month" name="endDate" id="endDate" />
+      <div className="grid grid-cols-2 gap-3">
+        <div className="flex flex-col">
+          <label htmlFor="job">Job Title</label>
+          <input
+            className="outline-none p-1 border-2 border-slate-700 rounded-sm"
+            type="text"
+            name="job"
+            id="job"
+            placeholder="Sr. Accountant"
+          />
+          <label htmlFor="employer">Employer</label>
+          <input
+            className="outline-none p-1 border-2 border-slate-700 rounded-sm"
+            type="text"
+            name="employer"
+            id="employer"
+            placeholder="Monster Inc."
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="cityProvince">City Province</label>
+          <input
+            className="outline-none p-1 border-2 border-slate-700 rounded-sm"
+            type="text"
+            name="cityProvince"
+            id="cityProvince"
+            placeholder="DKI Jakarta"
+          />
+          <label htmlFor="country">Country</label>
+          <input
+            className="outline-none p-1 border-2 border-slate-700 rounded-sm"
+            type="text"
+            name="country"
+            id="country"
+            placeholder="Indonesia"
+          />
+        </div>
+
+        <div className="col-span-2 flex flex-col">
+          <label htmlFor="startDate">Start Date</label>
+          <input
+            className="outline-none p-1 border-2 border-slate-700 rounded-sm"
+            type="month"
+            name="startDate"
+            id="startDate"
+          />
+          <label htmlFor="endDate">End Date</label>
+          <input
+            className="outline-none p-1 border-2 border-slate-700 rounded-sm"
+            type="month"
+            name="endDate"
+            id="endDate"
+          />
+        </div>
       </div>
     </form>
   );
