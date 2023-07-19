28d63624c4501cd8e28b36d1d5e7e1559a0e35fc
yhuzued <yusufmait@gmail.com>
2023-07-19 10:43:57 +0700

    Hide all button when print

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'print': { 'raw': 'print' },
+      }
+    },
   },
   plugins: [],
 }
