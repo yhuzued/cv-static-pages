54ef0318647322bf2c3929744890ad33106d5b29
yhuzued <yusufmait@gmail.com>
2023-07-23 10:46:49 +0700

    adjust style

--- src/components/skills/Title.jsx
+++ src/components/skills/Title.jsx
@@ -6,7 +6,7 @@ Title.propTypes = {
 
 export default function Title(props) {
   return (
-    <div className="flex justify-between w-full border-b-2 items-center">
+    <div className="flex justify-between w-full border-b-4 items-center">
       <h2 className="text-2xl my-4 ">Skills</h2>
       {props.children}
     </div>
